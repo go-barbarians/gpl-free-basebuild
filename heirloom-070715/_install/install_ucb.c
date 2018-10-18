f4491b7b0aacf34acdb1ed2ee29af040c1327a2e
Rob Gibbon <rob@barbarians.io>
2018-10-18 15:34:36 +0000

    remove (accidentally) cached binaries and makefiles
    Signed-off-by: Rob Gibbon <rob@barbarians.io>

--- heirloom-070715/_install/install_ucb.c
+++ heirloom-070715/_install/install_ucb.c
@@ -32,7 +32,7 @@
 #else
 #define	USED
 #endif
-static const char sccsid[] USED = "@(#)/usr/ucb/install.sl	1.12 (gritter) 5/29/05";
+static const char sccsid[] USED = "@(#)/usr/bin/install.sl	1.12 (gritter) 5/29/05";
 
 #include	<sys/types.h>
 #include	<sys/stat.h>
